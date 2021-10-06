702113b6be7a03c3755f1e300d96a6215f5a1e2b
Asset <khaitbay96@gmail.com>
2021-10-06 11:21:33 +0500

    Added views and posts

--- anfisa/ice_cream/views.py
+++ anfisa/ice_cream/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Asset')
+
+
+def ice_cream_list(request):
+    return HttpResponse('Asset list')
+
+
+def ice_cream_detail(request, pk):
+    return HttpResponse(f'Asset detail: {pk}')
